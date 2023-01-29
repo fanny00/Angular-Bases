422af1d28be63f5aaef20a2e0786a30479c6275d
Fanny Campos <fannycamposa@gmail.com>
2023-01-29 18:10:57 -0300

    Directiva *ngFor

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
 })
-export class ListadoComponent implements OnInit {
- constructor() {
-  console.log('constructor');
- }
+export class ListadoComponent {
 
- ngOnInit(): void {
-   console.log('ngOnInit');
- }
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitán America'];
+  heroeBorrado: string = '';
 
+  borrarHereo(){
+    console.log('borrando...')
+    this.heroeBorrado = this.heroes.pop() || '';
+  }
 }
