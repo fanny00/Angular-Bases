da4b6cb486457cc5c51521c8d14d6955091a1f60
Fanny Campos <fannycamposa@gmail.com>
2023-01-29 13:37:26 -0300

    Metodos en el componente: acumular()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,7 @@ export class AppComponent {
   titulo: string = 'Contador App';
   numero: number = 10;
 
-  sumar() {
-    this.numero += 1;
-  }
-
-  restar() {
-    this.numero -= 1;
+  acumular( valor: number ) {
+    this.numero += valor;
   }
 }
