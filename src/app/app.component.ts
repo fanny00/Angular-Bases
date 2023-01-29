ef1f1eba82374e0fd8a2123321982209dce9240d
Fanny Campos <fannycamposa@gmail.com>
2023-01-29 13:33:02 -0300

    metodos en el componente 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   titulo: string = 'Contador App';
   numero: number = 10;
+
+  sumar() {
+    this.numero += 1;
+  }
+
+  restar() {
+    this.numero -= 1;
+  }
 }
