a3298c515c1f86265b8cbc1520e7c361884af9f7
Fanny Campos <fannycamposa@gmail.com>
2023-01-30 13:05:24 -0300

    FormsModule

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
 
+  agregar(){
+    console.log("Esta es una prueba");
+  }
 }
