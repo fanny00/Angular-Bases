1ca2159ce0093cd175ac562e99874a823f0cf104
Fanny Campos <fannycamposa@gmail.com>
2023-01-30 16:30:04 -0300

    Mostrar listado de personajes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -11,17 +11,33 @@ interface Personaje {
 })
 export class MainPageComponent {
 
+  // Arreglo
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 8500
+    }
+  ];
+
   // objeto
   nuevo: Personaje = {
     nombre: 'Trunks',
     poder: 14000
   }
 
-  // cambiarNombre( event: any ){
-  //   console.log( event.target.value );
-  // }
-
   agregar(){
+    if ( this.nuevo.nombre.trim().length === 0 ){
+      return;
+    }
     console.log(this.nuevo);
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
   }
 }
