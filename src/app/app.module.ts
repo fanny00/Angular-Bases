14f547e8493a5e7c0ec5a8496e074f37e29545e3
Fanny Campos <fannycamposa@gmail.com>
2023-01-29 22:18:23 -0300

    Modulos - segunda parte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
 import { heroesModule } from './heroes/heroes.module';
+import { contadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
   ],
   imports: [
     BrowserModule,
-    heroesModule
+    heroesModule,
+    contadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
