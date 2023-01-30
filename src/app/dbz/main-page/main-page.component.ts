ade6fa9a95f2d6d3d427dac59f10dc6023d74a67
Fanny Campos <fannycamposa@gmail.com>
2023-01-30 14:28:25 -0300

    ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
 
+  // objeto
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 14000
+  }
+
+  // cambiarNombre( event: any ){
+  //   console.log( event.target.value );
+  // }
+
   agregar(){
-    console.log("Esta es una prueba");
+    console.log(this.nuevo);
   }
 }
